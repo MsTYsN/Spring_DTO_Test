2570e21c36254a4c47bcd4bd3fd7e7d7457beb18
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2023-03-15 12:16:50 +0100

    added ModelMapper

--- src/main/java/com/example/dtotest/dto/UserLocationDTO.java
+++ src/main/java/com/example/dtotest/dto/UserLocationDTO.java
@@ -1,12 +1,11 @@
 package com.example.dtotest.dto;
 
+import com.example.dtotest.model.Location;
 import lombok.Data;
 
 @Data
 public class UserLocationDTO {
     private Long id;
     private String username;
-    private double latitude;
-    private double longitude;
-    private String place;
+    private Location location;
 }
--- src/main/java/com/example/dtotest/service/MappingService.java
+++ src/main/java/com/example/dtotest/service/MappingService.java
@@ -3,6 +3,7 @@ package com.example.dtotest.service;
 import com.example.dtotest.dto.UserLocationDTO;
 import com.example.dtotest.model.User;
 import com.example.dtotest.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +15,30 @@ public class MappingService {
     @Autowired
     private UserRepository userRepository;
 
+    private ModelMapper modelMapper = new ModelMapper();
+
     public List<UserLocationDTO> getAllUsersLocation() {
-        return userRepository
+        /*return userRepository
                 .findAll()
                 .stream()
                 .map(this::convertDataIntoDTO)
+                .collect(Collectors.toList());*/
+        return userRepository
+                .findAll()
+                .stream()
+                .map(user -> modelMapper.map(user, UserLocationDTO.class))
                 .collect(Collectors.toList());
     }
 
-    private UserLocationDTO convertDataIntoDTO(User user) {
+    /*private UserLocationDTO toDTO(User user) {
+        UserLocationDTO dto = modelMapper.map(user, UserLocationDTO.class);
+        dto.setLatitude(user.getLocation().getLatitude());
+        dto.setLongitude(user.getLocation().getLongitude());
+        dto.setPlace(user.getLocation().getPlace());
+        return dto;
+    }*/
+
+    /*private UserLocationDTO convertDataIntoDTO(User user) {
         UserLocationDTO dto = new UserLocationDTO();
         dto.setId(user.getId());
         dto.setUsername(user.getUsername());
@@ -30,5 +46,5 @@ public class MappingService {
         dto.setLongitude(user.getLocation().getLongitude());
         dto.setPlace(user.getLocation().getPlace());
         return dto;
-    }
+    }*/
 }
